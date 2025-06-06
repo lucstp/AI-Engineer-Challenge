e3133c7f22838eab81070f9d271ec63bae8d1329
Luc St-Pierre <devnull404me@protonmail.com>
2025-06-06 19:04:38 +0200

    feat(frontend): add UI state management and modern tooling

---
+++ frontend/prettier.config.js
@@ -0,0 +1,23 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: [
+    '^react$',
+    '^next',
+    '^@/components/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/app/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^[./]',
+    '.css$',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+  importOrderCaseSensitive: false,
+}; 

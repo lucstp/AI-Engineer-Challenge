7897c4d32c3cffce6198dd875328192e27337256
Luc St-Pierre <devnull404me@protonmail.com>
2025-06-08 20:11:46 +0200

    feat(chat): add barrel exports for clean public API

---
+++ frontend/src/features/chats/index.ts
@@ -0,0 +1,22 @@
+// Barrel exports for chat feature - clean public API
+export type {
+  Message,
+  ChatState,
+  ChatContextType,
+  ChatActionResult,
+  ChatFormState,
+  ChatApiResponse,
+  StreamingChatResponse,
+  ChatConfig,
+  MessageRole,
+  MessageContent,
+  OptimisticMessage,
+} from './types/chat'
+
+export { useChat } from './hooks/use-chat'
+export {
+  ChatProvider,
+  useChatContext,
+  withChatContext,
+  ChatContext
+} from './context/chat-context'
